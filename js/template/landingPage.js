78fe41c892a6d9a58f83d45d28ab7b27fb65a202
Metodi Deribanov <imaginois@gmail.com>
2017-06-06 12:01:54 -0700

    add q.js in lib. Bower dep not working https://stackoverflow.com/questions/23687267/q-2-0-0-installed-with-bower-causes-uncaught-referenceerror-require-is-not-defi

---
+++ js/template/landingPage.js
@@ -0,0 +1,22 @@
+define(function (require) {
+	'use strict';
+
+	var tpl = new (require('js/core/dom'));
+	
+
+	var template = tpl.h('div#view.wrapper.clearfix', [
+	  	tpl.h('aside', [
+	  		tpl.h('h3', ['Snabbdom That awkward Moment full movie'])
+  		]),	
+  		tpl.h('article.stripes', [
+  			tpl.h('section#continuewatching.clearfix', [
+		  		tpl.h('header', [tpl.h('i.fa.fa-camera-retro fa-4x'), 'Keep Watching']),
+		  		tpl.h('article', [tpl.h('div.items',[
+									tpl.h('div.image-wrapper', ['<img class="image" src="http://netflixlife.com/files/2015/05/inglourious-basterds-.jpg" alt="" />'])
+								])]),
+  			]),
+  		]),
+	]);
+    
+    return template;
+});
